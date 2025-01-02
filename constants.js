694bc2bbc9959fe4a834bb5ca70e9da0ac3b08bd
Mujeeb <itsmdmujeeb@gmail.com>
2025-01-02 18:42:56 +0530

    port removed

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 export const DB_URI = process.env.DB_URI + "/" + process.env.DB_NAME;
 export const PORT = process.env.PORT;
 export const BASE_URL = process.env.BASE_URL;
-export const SHORT_URL = process.env.SHORT_URL + ":" + PORT;
+export const SHORT_URL = process.env.SHORT_URL;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
